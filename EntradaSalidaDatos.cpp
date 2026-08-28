4e13c1106f09e01d7c88ad8bfb53b969a46c7d3a
Luis Gerardo López Ruiz <151033145+gerataker3000@users.noreply.github.com>
2026-08-28 15:31:13 -0600

    add

---
+++ EntradaSalidaDatos.cpp
@@ -0,0 +1,16 @@
+//
+// Created by LegoC on 28/08/2026.
+//
+#include <iostream>
+using namespace std;
+int main() {
+    int numero;
+    cout << "Numero de visitas al cine este año: ";
+    cin >> numero;
+    cout << "Numero dado" << numero << endl;
+    string nombre;
+    cout << "Quien visito: "<< endl;
+    getline(cin >> ws, nombre);
+    cout << "Nombre : " << nombre << endl;
+
+}
