8f5c9b90d56f03097e5e021a199ac7c23e1fe399
Luis Gerardo López Ruiz <151033145+gerataker3000@users.noreply.github.com>
2026-09-02 15:49:15 -0600

    add if simple if else

---
+++ IfSimple.cpp
@@ -0,0 +1,31 @@
+//
+// Created by LegoC on 02/09/2026.
+//
+#include <iostream>
+using namespace std;
+int main() {
+    int tienesCelular = 0;
+
+    if (tienesCelular == 1) {
+        cout << "Armar among us" << endl;
+    }
+
+
+
+    return 0;
+    //If simple entrar al laboratorio
+    // Diagrama de flujo :D
+    int edad;
+    bool diagramaFlujo;
+    cout << "Edad: " << endl;
+    cin >> edad;
+    cout << "Diagrama: " << endl;
+    cin >> boolalpha >> diagramaFlujo;
+    // AND &&
+    // OR ||
+
+    if (edad >= 18 && diagramaFlujo && edad <= 100) {
+        cout << "Bievenido al laboratorio de dexter :D" << endl;
+    }
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "Gracias a dios ya es viernes" ;
+    cout << "Gracias a dios ya es viernes" << endl;
+    cout << (10 > 2) << endl;
+    cout << (10 < 2) << endl;
+    cout << (10 == 2) << endl;
+    cout << (10 != 2) << endl;
+    cout << (10 >= 2) << endl;
+    cout << (10 <= 2) << endl;
     return 0;
 }
